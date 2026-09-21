5bcdcaffd59c0a7b530903a7de63c23bdd993418
Alam <alamthelibrarian@gmail.com>
2026-09-21 08:22:50 +0700

    fix: activate fresh PWA builds immediately

--- excalidraw-app/index.tsx
+++ excalidraw-app/index.tsx
@@ -7,9 +7,18 @@ import "../excalidraw-app/sentry";
 import ExcalidrawApp from "./App";
 
 window.__EXCALIDRAW_SHA__ = import.meta.env.VITE_APP_GIT_SHA;
+
 const rootElement = document.getElementById("root")!;
 const root = createRoot(rootElement);
-registerSW();
+
+let updateSW: ReturnType<typeof registerSW>;
+updateSW = registerSW({
+  immediate: true,
+  onNeedRefresh() {
+    void updateSW(true);
+  },
+});
+
 root.render(
   <StrictMode>
     <ExcalidrawApp />
